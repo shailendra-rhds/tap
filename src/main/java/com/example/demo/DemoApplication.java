e633b1168863e01150c6b20dd17333b8f58f02da
Shailendra Kumar Singh <shailendra14k@gmail.com>
2025-08-16 09:58:17 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -12,7 +12,7 @@ public class DemoApplication {
 
     @RequestMapping("/")
     String home() {
-        return "Hello World! shailendra14k-1";
+        return "Hello World! shailendra14k-2";
     }
 
     public static void main(String[] args) {
